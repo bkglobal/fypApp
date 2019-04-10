6742ba1eaf61feb2b93143d2ab7e71ef1108da01
muhammadbilal <khatribilal5@gmail.com>
2019-04-11 00:49:44 +0500

    added all bugs page

--- bug_triager_FE/src/app/layout/all-bugs/all-bugs.component.ts
+++ bug_triager_FE/src/app/layout/all-bugs/all-bugs.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,ViewChild } from '@angular/core';
+declare var $;
+
 
 @Component({
   selector: 'app-all-bugs',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-bugs.component.scss']
 })
 export class AllBugsComponent implements OnInit {
-
+  @ViewChild('dataTable') table;
+  dataTable: any;
+  options: any;
   constructor() { }
 
   ngOnInit() {
+    this.dataTable = $(this.table.nativeElement);
+    this.options = {
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    };
+    this.dataTable.DataTable(this.options);
+
   }
 
 }
